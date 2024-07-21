527d44f446b6d36a63f5477579dd138de89d651e
alonex101 <156335125+alonex101@users.noreply.github.com>
2024-07-22 00:07:28 +0700

    Update bendingspoonsapps.js

--- bendingspoonsapps.js
+++ bendingspoonsapps.js
@@ -1,24 +1,32 @@
-if ($response.body != 'undefined'){
 var chxm1023 = JSON.parse($response.body);
-const url = $request.url;
+const headers = $request.headers;
+const ua = headers['User-Agent'] || headers['user-agent'];
 
-if (url.indexOf('splice') != -1) { ids = "com.path36.SpliceFree.1y_t150_bundle";}
-if (url.indexOf('filmicpro') != -1) { ids = "com.cinegenix.filmic.pro.1y_t130_bundle_creator";}
-if (url.indexOf('firstlight') != -1) { ids = "com.filmicpro.firstlight.1y_t130_bundle_creator";}
-if (url.indexOf('doubletake') != -1) { ids = "com.filmicpro.doubletake.1w_t20_bundle_creator";}
-if (url.indexOf('focos') != -1) { ids = "com.focos.1y_t130_bundle_creator";}
-if (url.indexOf('remini') != -1) { ids = "com.bigwinepot.nwdn.international.1y_p99_99_ft_pro";}
-if (url.indexOf('focoslive') != -1) { ids = "com.focoslive.1y_t130_adj";}
-if (url.indexOf('thirtydayfitness') != -1) { ids = "com.vigorapps.30DayFitness.1y_t130_bundle_adj";}
-if (url.indexOf('sleep') != -1) { ids = "com.bendingspoonsapps.SleepHelp.1y_t100_bundle_adj";}
-if (url.indexOf('yoga') != -1) { ids = "com.flyingnayeem.yoga.1y_t100_1w_bundle_adj";}
+const list = {
+  'SpeakTranslate': { id: "com.speakandtranslate.premium.69_99.yearly.7dtrial.groupa" },  //SpeakTranslate-说话与翻译-翻译器
+  'splice': { id: "com.path36.SpliceFree.1y_t150_bundle" },  //Splice-视频编辑
+  'filmicpro': { id: "com.cinegenix.filmic.pro.1y_t130_bundle_creator" },  //Filmicpro-专业级摄影机
+  'firstlight': { id: "com.filmicpro.firstlight.1y_t130_bundle_creator" },  //Firstlight-照片应用
+  'doubletake': { id: "com.filmicpro.doubletake.1w_t20_bundle_creator" },  //Doubletake
+  'focos': { id: "com.focos.1y_t130_bundle_creator" },  //Focos-单反相机
+  'remini': { id: "com.bigwinepot.nwdn.international.1y_p99_99_ft_pro" },  //Remini-人工智能修图
+  'focoslive': { id: "com.focoslive.1y_t130_adj" },  //Focos live-视频编辑工具
+  'thirtydayfitness': { id: "com.vigorapps.30DayFitness.1y_t130_bundle_adj" },  //30 Day Fitness-30天减肥运动
+  'sleep': { id: "com.bendingspoonsapps.SleepHelp.1y_t100_bundle_adj" },  //Sleep-睡眠助手
+  'yoga': { id: "com.flyingnayeem.yoga.1y_t100_1w_bundle_adj" }  //Yoga-冥想瑜伽
+};
 
-chxm1023["me"]["active_subscriptions_ids"] = [(ids)];
-chxm1023["me"]["active_bundle_subscriptions"] = [{
-   "expiry" : "2099-09-09T09:09:09+00:00",
-   "product_id" : (ids),
-   "features" : ["unlock"]
-  }];
-chxm1023["settings"]["__identity__"]["expiration"] = "2099-09-09T09:09:09+00:00";
-$done({body : JSON.stringify(chxm1023)});
+for (const key in list) {
+  if (new RegExp(`^${key}`, `i`).test(ua)) {
+    chxm1023["me"]["active_subscriptions_ids"] = [list[key].id];
+    chxm1023["me"]["active_bundle_subscriptions"] = [{
+      "expiry": "2099-09-09T09:09:09+00:00",
+      "product_id": list[key].id,
+      "features": ["unlock"]
+    }];
+    chxm1023["settings"]["__identity__"]["expiration"] = "2099-09-09T09:09:09+00:00";
+    break;
+  }
 }
+
+$done({body: JSON.stringify(chxm1023)});
